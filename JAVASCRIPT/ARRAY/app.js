08ca04cd9120c71b0f2006f35e4936d04a8cd8cd
ortigolu <vpasuenolar@gmail.com>
2023-09-09 13:45:19 -0400

    First commit to save in my github and test it

---
+++ JAVASCRIPT/ARRAY/app.js
@@ -0,0 +1,38 @@
+//array of students
+let students = [];
+
+//array of colors
+let colors = ["red", "blue", "green", "yellow", "orange", "purple", "pink"];
+
+//array of numbers
+let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+//exammple of array index
+console.log(colors[0]);
+console.log(colors[1]);
+console.log(colors[2]);
+
+//modify an array
+colors[0] = "black";
+
+//push method
+colors.push("white");
+
+//pop method
+colors.pop();
+
+//shift method remove from the start
+colors.shift();
+
+//unshift method add to the start
+colors.unshift("brown");
+
+//concat method
+let colors2 = ["grey", "gold"];
+let allColors = colors.concat(colors2);
+
+//includes method
+console.log(colors.includes("red"));
+
+//indexOf method
+console.log(colors.indexOf("red"));
---
+++ JAVASCRIPT/RUNNING FROM A SCRIPT/app.js
@@ -0,0 +1,60 @@
+// const dayofweek = prompt("Enter a day of the week").toLowerCase();
+// if (dayofweek === "monday") {
+//   console.log("I hate mondays");
+// } else if (dayofweek === "friday") {
+//   console.log("I love fridays");
+// } else if (dayofweek === "saturday" || dayofweek === "sunday") {
+//   console.log("I love weekends");
+// } else if (dayofweek === "thursday") {
+//   console.log("I love thursdays");
+// } else if (dayofweek === "tuesday") {
+//   console.log("I love tuesdays");
+// } else {
+//   console.log("I love wednesdays");
+// }
+
+// const age = 8;
+
+// if (age < 18) {
+//   console.log("You are a child");
+// } else if (age >= 18 && age < 65) {
+//   console.log("You are an adult");
+// } else if (age >= 65) {
+//   console.log("You are a senior");
+// }
+// const phrase = "";
+// if (phrase === "stop") {
+//   console.log("red");
+// } else if (phrase === "slow") {
+//   console.log("yellow");
+// } else if (phrase === "go") {
+//   console.log("green");
+// } else {
+//   console.log("purple");
+//
+
+// const password = prompt("Enter a password");
+
+// if (password.length >= 6 && password.indexOf(" ") === -1) {
+//   console.log("Valid password");
+// } else {
+//   console.log("Invalid password");
+// }
+// if (password.length >= 6) {
+//   console.log("Valid password");
+// } else {
+//   console.log("Password too short");
+// }
+
+// if (password.indexOf(" ") === -1) {
+//   console.log("Valid password");
+// } else {
+//   console.log("Password cannot contain spaces");
+// }
+
+// const user = 0;
+// if (user) {
+//   console.log("True");
+// } else {
+//   console.log("False");
+// }
