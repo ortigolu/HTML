850c731ff4cdfb21fb4388bcf8ae04c3b1df6ccd
ortigolu <vpasuenola@gmail.com>
2023-09-30 08:59:45 -0400

    update with oop

---
+++ OOP_Code/OOP_Code/07_Extends_and_Super/app.js
@@ -0,0 +1,51 @@
+class Pet {
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+  }
+  eat() {
+    return `${this.name} is eating!`;
+  }
+}
+class Cat extends Pet {
+  meow() {
+    return "MEOW";
+  }
+}
+
+class Dog extends Pet {
+  bark() {
+    return "guaug";
+  }
+}
+
+// class Pet {
+// 	constructor(name, age) {
+// 		console.log('IN PET CONSTRUCTOR!');
+// 		this.name = name;
+// 		this.age = age;
+// 	}
+// 	eat() {
+// 		return `${this.name} is eating!`;
+// 	}
+// }
+
+// class Cat extends Pet {
+// 	constructor(name, age, livesLeft = 9) {
+// 		console.log('IN CAT CONSTRUCTOR!');
+// 		super(name, age);
+// 		this.livesLeft = livesLeft;
+// 	}
+// 	meow() {
+// 		return 'MEOWWWW!!';
+// 	}
+// }
+
+// class Dog extends Pet {
+// 	bark() {
+// 		return 'WOOOF!!';
+// 	}
+// 	eat() {
+// 		return `${this.name} scarfs his food!`;
+// 	}
+// }
