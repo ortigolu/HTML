045f697fac58d6806f504729ba5302bea646a383
ortigolu <vpasuenola@gmail.com>
2023-10-07 12:16:51 -0300

    Update with Templating

---
+++ Templating_Demo/index.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "/views"));
+
+app.get("/", (req, res) => {
+  res.render("home.ejs");
+});
+
+app.get("/cats", (req, res) => {
+  const cats = ["Blue", "Rocket", "Monty", "Stephanie", "Winston"];
+  res.render("cats.ejs", { cats });
+});
+
+app.get("/r/:subreddit", (req, res) => {
+  const { subreddit } = req.params;
+  res.render("subredit.ejs", { subreddit });
+});
+
+app.get("/rand", (req, res) => {
+  const num = Math.floor(Math.random() * 10) + 1;
+  res.render("render.ejs", { rand: num });
+});
+
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
